af90f0f67dd9a8fbc0b4b1943bbd651b73e6b91f
maescristro <Chrisdeparis@users.noreply.github.com>
2020-04-03 03:28:30 +0200

    Create nomcli03.sql

---
+++ TESTS/CHARGEDB2/nomcli03.sql
@@ -0,0 +1,11 @@
+-- script sql pour programme de TU :  nomcliTU
+
+-- cas 3 : client n° 4 en double dans table client --
+
+insert into t_nomclitu/client          
+(numclient, nomclient, prenomclient)
+values (4, 'BERNARD', 'Renée');
+
+insert into t_nomclitu/client          
+(numclient, nomclient, prenomclient)
+values (4, 'BERNARD', 'Renée');  
