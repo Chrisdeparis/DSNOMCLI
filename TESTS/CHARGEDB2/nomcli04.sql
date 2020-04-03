dd0996613de57c8bc4701b8c751321add8fad011
maescristro <Chrisdeparis@users.noreply.github.com>
2020-04-03 03:28:58 +0200

    Create nomcli04.sql

---
+++ TESTS/CHARGEDB2/nomcli04.sql
@@ -0,0 +1,19 @@
+-- script sql pour programme de TU :  nomcliTU
+
+-- cas 4 : recherche du client n° 5 qui n'est pas présent dans table client --
+
+insert into t_nomclitu/client          
+(numclient, nomclient, prenomclient)
+values (1, 'DUPONT', 'Maria');
+
+insert into t_nomclitu/client          
+(numclient, nomclient, prenomclient)
+values (2, 'JEAN', 'Ana');
+ 
+insert into t_nomclitu/client          
+(numclient, nomclient, prenomclient)
+values (3, 'MICHEL', 'Charlotte');
+
+insert into t_nomclitu/client          
+(numclient, nomclient, prenomclient)
+values (6, 'DUMANS', 'Henriette');
